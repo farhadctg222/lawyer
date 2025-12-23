c3a51d4ce920231ba5659fa0a89d36540c04eb23
farhadctg222 <farhadctg222@gmail.com>
2025-12-23 20:13:51 +0600

    d

--- app/componet/DataMainPageShow.jsx
+++ app/componet/DataMainPageShow.jsx
@@ -1,68 +1,122 @@
-import React from 'react';
+// 'use client'
+// import React from 'react';
+
+// import './Division.css'
+
+
+// const data = async () => {
+
+//     const res = await fetch('http://localhost:3000/api')
+
+//     const datres = await res.services.json()
+//     return datres
+
+// }
+
+// const DataMainPageShow = async () => {
+//     const lawdata = await data()
+//     console.log(lawdata)
 
 
 
 
-const data = async () => {
+//     return (
+//         <div>
+//   <h1>All Post</h1>
 
-    const res = await fetch('http://localhost:3000/api')
-
-    const datres = await res.json()
-    return datres
-
-}
-
-const DataMainPageShow = async () => {
-    const lawdata = await data()
-
-
-
-
-    return (
-        <div>
-  <h1>All Post</h1>
-
-  <div>
-    {lawdata?.map((post) => (
-      <div key={post.id}>
-        <div className="ProfileCard">
-          <h1
-            className="headerProfile"
-            style={{ border: "1px solid black" }}
-          >
-            <a
-              href="#"
-              title={`${post.name}, Zip: ${post.address?.zipcode}`}
-            >
-              {post.name}
-            </a>
-
-            <div className="image">
-              {/* image here */}
-            </div>
-
-            <div className="nameHeader">
-              <h3 style={{ color: "#2a7fba" }}>{post.name}</h3>
-
-              <h5>{post.phone}</h5>
-
-              <h5>{post.website}</h5>
-
-              <h5 className="desination">{post.email}</h5>
-            </div>
-          </h1>
-        </div>
-      </div>
-    ))}
-  </div>
-</div>
+//   <div>
+//     {lawdata?.map((post) => (
+//       <div className='ProfileCard'>
+//             <h1 className="headerProfile"style={{border:'1px solid black'}}>
+//             <title>{post.name} <br />{post.Education} <br />{post.Desination}</title>  
+//               <a href=""><title>{post.ChamberName} <br />{post.workPlace}</title></a>
+//             <div className="image">
+//                 <img src={post.image} alt="image" />
+//                </div>
+//                <div className="nameHeader">
+//                <h3 style={{color:'#2a7fba',}} >{post.name}</h3>
+//                 <h5 >{post.Education}</h5>
+//                 <h6 style={{color:'red',fontFamily:'robute,sans-serif'}}>{post.Specialist}</h6>
+//                 <h5 >{Desination}</h5>
+//                 <h5 className='desination' >{workPlace}</h5>
+     
+//                 <Link to={`/${space}`} style={{textDecoration:'none',color:'white'}}>  <button className='seeChamber' style={{textDecoration:'none',color:'white'}}  >See Chamber</button> </Link>
+//                </div></h1>
+              
+              
+             
+             
+            
+//         </div>
+//     ))}
+//   </div>
+// </div>
 
 
 
 
 
                         
-    );
+//     );
+// };
+
+// export default DataMainPageShow;
+
+import Link from "next/link";
+import './Division.css'
+
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api", {
+    cache: "no-store",
+    revalidate: 10,
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+const DataMainPageShow = async () => {
+  const lawdata = await getData();
+  const services = lawdata?.services || [];
+
+  return (
+    <div>
+      <h1>All Post</h1>
+
+      <div>
+        {services.map((post, index) => {
+          const slug = post.name
+            .replace(/\s+/g, "-")
+            .toLowerCase();
+
+          return (
+            <div className="ProfileCard" style={{border:'1px solid black', textAlign:'center', backgroundColor:'#f0f0f0'}} key={index}>
+              <div className="image">
+                <img src={post.image} alt={post.name} />
+              </div>
+
+              <div className="nameHeader">
+                <h1 style={{ color: "#2a7fba", fontWeight: "bold" }}>{post.name}</h1>
+                <h5>{post.Education}</h5>
+                <h6 style={{ color: "red" }}>{post.Specialist}</h6>
+                <h5>{post.Desination}</h5>
+                <h5 className="desination">{post.workPlace}</h5>
+
+                <Link href={`/lawyer/${slug}`}>
+                  <button className="seeChamber">
+                    See Chamber
+                  </button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default DataMainPageShow;
